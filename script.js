79f458ad681fe31f282034e2ec70e323916997b2
moi <diachabane@gmail.com>
2024-10-04 11:38:55 +0200

    achat_de_billet

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculerPrix() {
+  const activite = document.getElementById("activity").value;
+
+  const nbAdulte = document.getElementById("nbAdulte").value;
+  const nbEnfant = document.getElementById("nbEnfant").value;
+
+  const prixTotal = nbAdulte * activite + nbEnfant * (activite - 3);
+
+  document.getElementById("prix").value = prixTotal + "€";
+}
+
+console.log("slt");
